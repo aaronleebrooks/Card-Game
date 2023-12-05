62df433137b1111e62c996eec1c6c6f5282f4013
Aaron Brooks <pleaseshutupaaron@gmail.com>
2023-12-04 21:22:15 -0500

    hand controller

---
+++ Assets/Scripts/Hand/HandController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandController : MonoBehaviour
+{
+
+    public List<Card> handOfCards = new List<Card>();
+    public Transform minPosition;
+    public Transform maxPosition;
+    public List<Vector3> cardPositions = new List<Vector3>();
+
+    void Start()
+    {
+        SetCardPositionsInHand();
+    }
+
+    public void SetCardPositionsInHand()
+    {
+        cardPositions.Clear();
+        float distanceBetweenCards = (maxPosition.position.x - minPosition.position.x) / (handOfCards.Count - 1);
+        for (int i = 0; i < handOfCards.Count; i++)
+        {
+            Vector3 cardPosition = new Vector3(minPosition.position.x + (distanceBetweenCards * i), minPosition.position.y, minPosition.position.z);
+            cardPositions.Add(cardPosition);
+
+            handOfCards[i].transform.position = cardPosition;
+        }
+    }
+}
